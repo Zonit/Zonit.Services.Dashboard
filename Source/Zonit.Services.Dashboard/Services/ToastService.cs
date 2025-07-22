c6fbe806be5f6ab37d5ac6c80992cfa66d6112aa
Alerinos <42449535+Alerinos@users.noreply.github.com>
2025-07-22 14:22:15 +0200

    Poprawki w nawigacji i lokalizacji komunikatów toast
    Zmieniono ścieżkę URL dla elementu "Logout" w `MainLayout.razor` na małe litery.
    W `Routes.razor` uproszczono renderowanie widoków nieautoryzowanych, zastępując `MudContainer` z `LayoutView` i `MinimalLayout`.
    W `ToastService.cs` dodano wsparcie dla tłumaczenia komunikatów toast poprzez nowy parametr `ICultureProvider`.

--- Source/Zonit.Services.Dashboard/Services/ToastService.cs
+++ Source/Zonit.Services.Dashboard/Services/ToastService.cs
@@ -1,14 +1,18 @@
 ﻿using MudBlazor;
+using Zonit.Extensions.Cultures;
 using Zonit.Extensions.Website;
 using Zonit.Extensions.Website.Abstractions.Toasts.Types;
 
 namespace Zonit.Services.Dashboard.Services;
 
-internal class ToastService(ISnackbar snackbar) : IToastProvider
+internal class ToastService(
+        ISnackbar snackbar,
+        ICultureProvider culture
+    ) : IToastProvider
 {
     public void Add(string message, ToastType taskType)
     {
-        switch(taskType)
+        switch (taskType)
         {
             case ToastType.Info:
                 snackbar.Add(message, Severity.Info);
@@ -21,6 +25,29 @@ internal class ToastService(ISnackbar snackbar) : IToastProvider
                 break;
             case ToastType.Error:
                 snackbar.Add(message, Severity.Error);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(taskType), taskType, null);
+        }
+    }
+
+    public void Add(ToastType taskType, string message, params object?[] objects)
+    {
+        var translate = culture.Translate(message, objects);
+
+        switch (taskType)
+        {
+            case ToastType.Info:
+                snackbar.Add(translate, Severity.Info);
+                break;
+            case ToastType.Success:
+                snackbar.Add(translate, Severity.Success);
+                break;
+            case ToastType.Warning:
+                snackbar.Add(translate, Severity.Warning);
+                break;
+            case ToastType.Error:
+                snackbar.Add(translate, Severity.Error);
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(taskType), taskType, null);
